e6e027d703bce716566093536d6521fe368af8ec
TheRealLorenz <lollobellina@gmail.com>
2026-09-11 16:39:59 +0200

    feat: linux support for vimtex

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -1,19 +1,24 @@
 vim.pack.add { 'https://github.com/lervag/vimtex' }
 
-vim.g.vimtex_view_method = 'skim'
-vim.g.vimtex_view_skim_sync = 1
-vim.g.vimtex_view_skim_activate = 1
-vim.g.vimtex_compiler_method = 'latexmk'
-vim.g.vimtex_compiler_latexmk = {
-  continuous = 1,
-  options = {
-    '-pdf',
-    '-shell-escape',
-    '-verbose',
-    '-file-line-error',
-    '-synctex=1',
-    '-interaction=nonstopmode',
-  },
-}
--- vim.g.vimtex_quickfix_mode = 0 -- don't steal focus on warnings
--- vim.g.vimtex_quickfix_open_on_warning = 0
+if vim.fn.has 'macunix' == 1 then
+  vim.g.vimtex_view_method = 'skim'
+else
+  vim.g.vimtex_view_method = 'zathura'
+end
+
+-- vim.g.vimtex_view_skim_sync = 1
+-- vim.g.vimtex_view_skim_activate = 1
+
+-- vim.g.vimtex_compiler_method = 'latexmk'
+-- vim.g.vimtex_compiler_latexmk = {
+--   continuous = 1,
+--   options = {
+--     '-pdf',
+--     '-shell-escape',
+--     '-verbose',
+--     '-file-line-error',
+--     '-synctex=1',
+--     '-interaction=nonstopmode',
+--     '-recorder',
+--   },
+-- }
